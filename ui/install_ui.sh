abb458ec34ee430cf8e1900bd8780f6aa9b488a5
jackma <xiatian@118.online>
2026-01-30 06:45:13 +0700

    init

--- ui/install_ui.sh
+++ ui/install_ui.sh
@@ -14,20 +14,32 @@ cp -af "$TMP_UI/themes/Orchis-Dark/gtk-4.0" "$KDP/.config/"
 
 # 复制图标
 tar -xzvf "$TMP_UI/icons/icons.tar.gz" -C /usr/share/icons/
+# 更新图标缓存：这对于图标显示至关重要
+find /usr/share/icons -maxdepth 1 -type d -exec gtk-update-icon-cache -f -t {} \; 2>/dev/null || true
+
 
 # 复制壁纸
 cp -af "$TMP_UI/wallpapers/." /usr/share/backgrounds/
 
-# 复制配置
+# 复制字体
+FONT_DEST="/usr/share/fonts/truetype/apple-fonts"
+mkdir -p "$FONT_DEST"
+cp -af "$TMP_UI/fonts/." "$FONT_DEST/"
+chmod 644 "$FONT_DEST"/*
+fc-cache -f
+
+# 最后复制配置
 cp -af "$TMP_UI/kasm-default-profile/." "$KDP/"
 
 
+# 确保 genmon 脚本具有执行权限
+#chmod +x "$KDP/.config/xfce4/genmon/"*.sh 2>/dev/null || true
+
+
+
+
 # 复制字体
-#FONT_DEST="/usr/share/fonts/truetype/apple-fonts"
-#mkdir -p "$FONT_DEST"
-#cp -f /tmp/theme/fonts/*.ttf "$FONT_DEST/"
-#cp -f /tmp/theme/fonts/*.ttc "$FONT_DEST/"
-#chmod 644 "$FONT_DEST"/*
+
 #cat > /etc/fonts/conf.d/99-apple-fonts.conf <<EOF
 #<?xml version="1.0"?>
 #<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
