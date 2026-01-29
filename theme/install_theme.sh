57245bf0835f13e4295d5407e69bd0f83d8562c5
jackma <xiatian@118.online>
2026-01-29 18:19:31 +0700

    init

--- theme/install_theme.sh
+++ theme/install_theme.sh
@@ -6,3 +6,7 @@ cp -r /tmp/theme/Orchis-Dark* /usr/share/themes/
 mkdir -p /home/kasm-default-profile/.config/gtk-4.0
 cp -r /tmp/theme/Orchis-Dark/gtk-4.0/* /home/kasm-default-profile/.config/gtk-4.0/
 
+# 打入 xfce xml 配置
+DEST="/home/kasm-default-profile/.config/xfce4/xfconf/xfce-perchannel-xml"
+mkdir -p "$DEST"
+cp -f /tmp/theme/xfce-config/*.xml "$DEST/"
