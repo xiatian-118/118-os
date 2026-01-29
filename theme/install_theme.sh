2bf907eaa598d5da12e6cf1fabead71ebbf083cc
jackma <xiatian@118.online>
2026-01-29 23:39:05 +0700

    init

--- theme/install_theme.sh
+++ theme/install_theme.sh
@@ -16,38 +16,38 @@ mkdir -p "$FONT_DEST"
 cp -f /tmp/theme/fonts/*.ttf "$FONT_DEST/"
 cp -f /tmp/theme/fonts/*.ttc "$FONT_DEST/"
 chmod 644 "$FONT_DEST"/*
-cat > /etc/fonts/conf.d/99-apple-fonts.conf <<EOF
-<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-  <match target="font">
-    <edit name="antialias" mode="assign"><bool>true</bool></edit>
-    <edit name="hinting" mode="assign"><bool>true</bool></edit>
-    <edit name="hintstyle" mode="assign"><const>hintslight</const></edit>
-    <edit name="rgba" mode="assign"><const>rgb</const></edit>
-    <edit name="lcdfilter" mode="assign"><const>lcddefault</const></edit>
-  </match>
-
-  <alias>
-    <family>sans-serif</family>
-    <prefer>
-      <family>SF Pro</family>
-      <family>PingFang SC</family>
-      <family>Thonburi</family>
-      <family>.ThonburiUI</family>
-      <family>Noto Sans CJK SC</family>
-    </prefer>
-  </alias>
-
-  <match target="pattern">
-    <test name="family"><string>Microsoft YaHei</string></test>
-    <edit name="family" mode="assign" binding="strong">
-      <string>PingFang SC</string>
-    </edit>
-  </match>
-</fontconfig>
-EOF
-fc-cache -fv
+#cat > /etc/fonts/conf.d/99-apple-fonts.conf <<EOF
+#<?xml version="1.0"?>
+#<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+#<fontconfig>
+#  <match target="font">
+#    <edit name="antialias" mode="assign"><bool>true</bool></edit>
+#    <edit name="hinting" mode="assign"><bool>true</bool></edit>
+#    <edit name="hintstyle" mode="assign"><const>hintslight</const></edit>
+#    <edit name="rgba" mode="assign"><const>rgb</const></edit>
+#    <edit name="lcdfilter" mode="assign"><const>lcddefault</const></edit>
+#  </match>
+#
+#  <alias>
+#    <family>sans-serif</family>
+#    <prefer>
+#      <family>SF Pro</family>
+#      <family>PingFang SC</family>
+#      <family>Thonburi</family>
+#      <family>.ThonburiUI</family>
+#      <family>Noto Sans CJK SC</family>
+#    </prefer>
+#  </alias>
+#
+#  <match target="pattern">
+#    <test name="family"><string>Microsoft YaHei</string></test>
+#    <edit name="family" mode="assign" binding="strong">
+#      <string>PingFang SC</string>
+#    </edit>
+#  </match>
+#</fontconfig>
+#EOF
+#fc-cache -fv
 
 # 打入 xfce xml 配置
 DEST="/home/kasm-default-profile/.config/xfce4/xfconf/xfce-perchannel-xml"
