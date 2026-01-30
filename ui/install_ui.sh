b208bcd485f1987b680d2ff424aa0b4b44ba3f26
jackma <xiatian@118.online>
2026-01-30 20:14:37 +0700

    init

--- ui/install_ui.sh
+++ ui/install_ui.sh
@@ -7,16 +7,13 @@ TMP_UI="/tmp/ui"
 
 # 安装依赖
 cd "${TMP_UI}/deb"
-dpkg -i libdisplay-info3_0.3.0-1_amd64.deb
-dpkg -i libxfce4windowing-common_4.20.5-1_all.deb
-dpkg -i --force-depends libxfce4windowing-0-0_4.20.5-1_amd64.deb
-dpkg -i --force-depends xfce4-docklike-plugin_0.5.0-1_amd64.deb
+#dpkg -i libdisplay-info3_0.3.0-1_amd64.deb
+#dpkg -i libxfce4windowing-common_4.20.5-1_all.deb
+#dpkg -i --force-depends libxfce4windowing-0-0_4.20.5-1_amd64.deb
+#dpkg -i --force-depends xfce4-docklike-plugin_0.5.0-1_amd64.deb
+dpkg -i --force-depends xfce4-docklike-plugin_0.4.3-1_amd64.deb
 ldconfig
 apt-mark hold xfce4-docklike-plugin
-apt-mark hold libxfce4windowing-0-0
-apt-mark hold libxfce4windowing-common
-apt-mark hold libdisplay-info3
-
 
 
 
